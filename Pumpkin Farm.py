f6dea893e0c5a93fa30b3cefae75465e617552d9
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-08-28 21:07:32 +0100

    Implemented Stop Condition and fixed format

--- Pumpkin Farm.py
+++ Pumpkin Farm.py
@@ -1,37 +1,43 @@
 clear()
 Dead_Pumpkin_Found = True
-while True:
+while num_items(Items.Carrot) != 0:
 
-    for i in range(get_world_size()):
+	for i in range(get_world_size()):
 
-        for j in range(get_world_size()):
+		for j in range(get_world_size()):
 
 
-            if get_entity_type() != Entities.Pumpkin:
+			if get_entity_type() != Entities.Pumpkin:
 
-                if get_entity_type() == Entities.Dead_Pumpkin:
-                    Dead_Pumpkin_Found = True
-                    plant(Entities.Pumpkin)
-                else:
-                    if get_ground_type() == Grounds.Grassland:
-                        till()
-                    plant(Entities.Pumpkin)
+				if get_entity_type() == Entities.Dead_Pumpkin:
+					Dead_Pumpkin_Found = True
+					plant(Entities.Pumpkin)
+				else:
+					if get_ground_type() == Grounds.Grassland:
+						till()
+					plant(Entities.Pumpkin)
 
-            # water every step (Due to waiting time for big pumpkin, water not worth it)
-            #if get_water() < 0.7:
-                #use_item(Items.Water)
+                
+			if num_items(Items.Carrot) == 0:
+				break
+                        
+			move(North)
 
-            move(North)
+		if num_items(Items.Carrot) == 0:
+			break
+            
+		move(East)
+	print("1 map loop complete")
 
-        move(East)
-    print("1 map loop complete")
-
-    # if no dead found and can harvest then harvest
-    if Dead_Pumpkin_Found == False and can_harvest():
-        harvest()
+	# if no dead found and can harvest then harvest
+	if Dead_Pumpkin_Found == False and can_harvest():
+		harvest()
 
     # reset Found to False 
-    Dead_Pumpkin_Found = False
+	Dead_Pumpkin_Found = False
+
+print("Carrot Stock is empty")
+    
 
 # Notes: Bug with harvesting first few pumpkins on grid even if
 # pumpkin is not dead. Implementation needs debugging then
