679653dc0434d5507fd0280d0cdd14d50ef21ddb
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-09-01 14:18:43 +0100

    Refactor plant_crop function and improve bubble sort implementation

--- Cactus Farm.py
+++ Cactus Farm.py
@@ -1,38 +1,59 @@
 clear()
-
 change_hat(Hats.Cactus_Hat)
 
-North_array = []
-East_array = [] 
+def plant_crop(crop):
 
-
-while num_items(Items.Pumpkin) > 1:
-
-	for i in range(3):
-
-		for j in range(3):
-
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
 			if get_ground_type() != Grounds.Soil:
 				till()
-
-
-			plant(Entities.Cactus)
-
-			if can_harvest():
-				harvest()
-
+			plant(crop)
 			move(North)
-
-		move(South)
-		move(South)
-		move(South)
 		move(East)
 
-	move(West)
-	move(West)
-	move(West)
 
-	# Implement Bubble sort as matches ingame implementation
-	# bubble sort rows then columns? As apparently if sort rows first wont change order when columns sorted
+plant_crop(Entities.Cactus)
 
-	break
+# sort using bubble sort
+
+# sort row 1
+
+n = get_world_size()
+# loop through row
+
+for i in range(n):
+	swapped = False
+
+	# bubble sort inner loop
+	for j in range(0, n): # try 0, n - i -1
+
+		# check we have cactus, always return integers
+		if get_entity_type() == Entities.Cactus:
+			# measure crop and crop to the east
+			x = measure()
+			y = measure(East)
+
+		# if current is bigger, swap cactus
+		if x > y: # type: ignore
+			# swap
+			swap(East)
+			swapped = True
+		move(East)
+
+	# if sorted stop
+	if swapped == False:
+		break
+
+
+# Debug Notes:
+# inner loop j, for bubble sort when using 0, n-i-1
+# Not completed sort, missing few smaller cactus
+# However 0,n gets them all so need debugging
+#	
+#
+
+
+
+
+
+# sort columns
