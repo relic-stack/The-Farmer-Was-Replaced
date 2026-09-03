5cd2640dff692b075337169aa8f4747e784cde86
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-09-03 12:46:48 +0100

    Refactor planting and sorting functions for clarity and organization

--- Cactus Farm.py
+++ Cactus Farm.py
@@ -1,7 +1,10 @@
 clear()
 change_hat(Hats.Cactus_Hat)
 
-def plant_crop(crop):
+
+
+# function to plant entire world
+def plant_crop_world(crop):
 
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
@@ -12,52 +15,62 @@ def plant_crop(crop):
 		move(East)
 
 
-plant_crop(Entities.Cactus)
 
-
-def move_done(direction,n):
+# function to move direction n times
+def move_drone(direction,n):
 	for i in range(n):
 		move(direction)
 
-# sort using bubble sort
-
-# sort row 1
-
-n = get_world_size()
-# loop through row
-
-for i in range(n):
-	swapped = False
-
-	# bubble sort inner loop
-	for j in range(0, n-i-1): # try 0, n - i -1
-
-		# check we have cactus, always return integers
-		if get_entity_type() == Entities.Cactus:
-			# measure crop and crop to the east
-			x = measure()
-			y = measure(East)
-
-		# if current is bigger, swap cactus
-		if x > y: # type: ignore
-			# swap
-			swap(East)
-			swapped = True
-		move(East)
 
 
-	# move east i times (reset back to start of row)
-	# i+1 due to i starting at 0
-	move_done(East, i+1)
+# Bubble Sort Implementation in chosen direction e.g East = Row, North = Column
+def bubble_sort(direction):
 
-	# if sorted stop
-	if swapped == False:
-		break
+	n = get_world_size()
 
-# sort columns
+	# outer loop
+	for i in range(n):
+		swapped = False
+
+		# bubble sort inner loop
+		for j in range(0, n-i-1):
+
+			# check we have cactus, always return integers
+			if get_entity_type() == Entities.Cactus:
+				# measure crop and crop to the east
+				x = measure()
+				y = measure(direction)
+
+			# if cactus is bigger, swap cactus
+			if x > y: # type: ignore
+				swap(direction)
+				swapped = True
+			move(direction)
+
+		# move east i+1 times (reset back to start of row)
+		move_drone(direction, i+1)
+
+		# if sorted stop
+		if swapped == False:
+			break
+
+
+
+plant_crop_world(Entities.Cactus)
+
+# bubble sort every row in world 
+for i in range(get_world_size()):
+	bubble_sort(East)
+	move(North)
+
+# bubble sort every column in world
+for i in range(get_world_size()):
+	bubble_sort(North)
+	move(East)
+
+
 
 
 # Notes
-
 # next step is to put bubble sort code into function so it can run on each row
 # then on each column to get full sorted grid In direction NE
