e623d80af489a6aebe0844862942fe2baed360a7
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-09-04 11:14:31 +0100

    Implement basic fertilizer farm

---
+++ Fertilizer Farm.py
@@ -0,0 +1,13 @@
+clear()
+
+while True:
+
+    for i in range(get_world_size()):
+
+        use_item(Items.Fertilizer)
+
+        if can_harvest():
+            harvest()
+
+        move(North)
+    move(East)
