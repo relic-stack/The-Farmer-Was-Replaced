e3a594c25e26174026eb7c70f680b4757e35c67e
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-09-04 12:56:19 +0100

    Add Basic Fertilizer Farm

--- Fertilizer Farm.py
+++ Fertilizer Farm.py
@@ -2,12 +2,12 @@ clear()
 
 while True:
 
-    for i in range(get_world_size()):
+	for i in range(get_world_size()):
 
-        use_item(Items.Fertilizer)
+		use_item(Items.Fertilizer)
 
-        if can_harvest():
-            harvest()
+		if can_harvest():
+			harvest()
 
-        move(North)
-    move(East)
+		move(North)
+	move(East)
