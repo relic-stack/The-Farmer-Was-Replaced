f4f6ecbc9abd8f28aee414f1fe2d09bc0034060b
Ben Gilroy <115704549+relic-stack@users.noreply.github.com>
2026-09-07 06:31:52 +0100

    Refactor planting logic and track every flower position and value

--- Sunflower Farm.py
+++ Sunflower Farm.py
@@ -1,7 +1,3 @@
-clear()
-
-set_world_size(6)
-
 def move_to_pos(x,y):
     # get current pos x pos y
     # input = target x,y
@@ -9,21 +5,35 @@ def move_to_pos(x,y):
     # find difference
     pass
 
+def add_flower_info(sunflowers):
+    x = get_pos_x()
+    y = get_pos_y()
+    val = measure()
+    flower = [val, x, y]
+    sunflowers.append(flower)
+
+def Initial_Plant(sunflowers):
+
+    for _ in range(get_world_size()):
+        for _ in range(get_world_size()):
+
+            if get_ground_type() == Grounds.Grassland:
+                till()
+            
+            plant(Entities.Sunflower)
+            add_flower_info(sunflowers)
+
+            move(North)
+        move(East)
 
 
 
+clear()
+set_world_size(6)
 
 
-
-for i in range(get_world_size()):
-    for j in range(get_world_size()):
-
-        if can_harvest():
-            harvest()
-
-        if get_ground_type() == Grounds.Grassland:
-            till()
-
-        plant(Entities.Sunflower)
-        move(North)
-    move(East)
+sunflowers = []
+Initial_Plant(sunflowers)
+print(sunflowers)
+# max uses first value in list, so val is first
+print(max(sunflowers)) #[15,5,3] [val,x,y]
